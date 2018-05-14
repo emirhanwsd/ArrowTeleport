8c166bab6f73a6f364fe45205172fb0468d2af99
Emirhan Akpınar <leetpocketmine@gmail.com>
2018-05-14 17:10:34 +0300

    Added new functions and new projectiles

--- ArrowTeleport/src/ArrowTeleport/EventListener.php
+++ ArrowTeleport/src/ArrowTeleport/EventListener.php
@@ -7,22 +7,63 @@ use pocketmine\event\entity\ProjectileHitEvent;
 
 use pocketmine\Player;
 
-use pocketmine\entity\projectile\Arrow;
+use pocketmine\entity\projectile\{Arrow, Egg, Snowball;
 
 use pocketmine\level\Level;
 use pocketmine\level\sound\EndermanTeleportSound;
 
+use ArrowTeleport\Main;
+
 class EventListener implements Listener {
 
-	public function teleport(ProjectileHitEvent $e) {
+	public function arrowTeleport(ProjectileHitEvent $e) {
 		$entity = $e->getEntity();
 		if($entity instanceof Arrow) {
-			$player = $entity->getOwningEntity();
-			if($player instanceof Player) {
-				$player->teleport($entity);
-				$sound = new EndermanTeleportSound($player);
-				$player->getLevel()->addSound($sound);
-				
+			if(Main::getAPI()->config->get("projectile") == "Arrow") {
+				$player = $arrow->getOwningEntity();
+				if($player instanceof Player) {
+					$player->teleport($entity);
+					$sound = new EndermanTeleportSound($player);
+					$player->getLevel()->addSound($sound);
+					
+				}
+			
+			}
+			
+		}
+		
+	}
+	
+	public function eggTeleport(ProjectileHitEvent $e) {
+		$entity = $e->getEntity();
+		if($entity instanceof Egg) {
+			if(Main::getAPI()->config->get("projectile") == "Egg") {
+				$player = $arrow->getOwningEntity();
+				if($player instanceof Player) {
+					$player->teleport($entity);
+					$sound = new EndermanTeleportSound($player);
+					$player->getLevel()->addSound($sound);
+					
+				}
+			
+			}
+			
+		}
+		
+	}
+	
+	public function snowballTeleport(ProjectileHitEvent $e) {
+		$entity = $e->getEntity();
+		if($entity instanceof Snowball) {
+			if(Main::getAPI()->config->get("projectile") == "Snowball") {
+				$player = $arrow->getOwningEntity();
+				if($player instanceof Player) {
+					$player->teleport($entity);
+					$sound = new EndermanTeleportSound($player);
+					$player->getLevel()->addSound($sound);
+					
+				}
+			
 			}
 			
 		}
